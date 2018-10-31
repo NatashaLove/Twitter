e16ec61af3e72c718f31d0756b1f8628f5b68f4e
Natasha S Love <debora@inbox.ru>
2018-10-31 12:43:16 -0500

    update

--- Twitter/Program.cs
+++ Twitter/Program.cs
@@ -9,6 +9,22 @@ namespace Twitter
     {
         static void Main(string[] args)
         {
+            string message = null;
+            Console.WriteLine("Hello, write your message (140 characters):");
+            message= Console.ReadLine();
+
+            while (message.Length>140)
+            {
+                Console.WriteLine("Your message is too long, try again.");
+                message = Console.ReadLine();
+            }
+           
+            {
+                Console.WriteLine("Your message is:");
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+
         }
     }
 }
